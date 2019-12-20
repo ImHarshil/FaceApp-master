b33b4bea4c09bf102bdee95578f2960cf0dfdff1
Harshil Rathod <50371099+ImHarshil@users.noreply.github.com>
2019-12-20 21:25:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/faceapp/AuthenticationActivity.java
@@ -0,0 +1,77 @@
+package com.example.faceapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.multidots.fingerprintauth.AuthErrorCodes;
+import com.multidots.fingerprintauth.FingerPrintAuthCallback;
+import com.multidots.fingerprintauth.FingerPrintAuthHelper;
+
+public class AuthenticationActivity extends Activity implements FingerPrintAuthCallback {
+    FingerPrintAuthHelper mFingerPrintAuthHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        mFingerPrintAuthHelper = FingerPrintAuthHelper.getHelper(this, (FingerPrintAuthCallback) this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //start finger print authentication
+        mFingerPrintAuthHelper.startAuth();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFingerPrintAuthHelper.stopAuth();
+    }
+
+    @Override
+    public void onNoFingerPrintHardwareFound() {
+        Toast.makeText(getApplicationContext(),"No FingerPrint is not supported in this device",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+    }
+
+    @Override
+    public void onNoFingerPrintRegistered() {
+
+    }
+
+    @Override
+    public void onBelowMarshmallow() {
+        Toast.makeText(getApplicationContext(),"Android version below Marshmallow",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+
+    @Override
+    public void onAuthSuccess(FingerprintManager.CryptoObject cryptoObject) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onAuthFailed(int errorCode, String errorMessage) {
+        switch (errorCode) {    //Parse the error code for recoverable/non recoverable error.
+            case AuthErrorCodes.CANNOT_RECOGNIZE_ERROR:
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+                //Cannot recognize the fingerprint scanned.
+                break;
+            case AuthErrorCodes.NON_RECOVERABLE_ERROR:
+                //This is not recoverable error. Try other options for user authentication. like pin, password.
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+
+                break;
+            case AuthErrorCodes.RECOVERABLE_ERROR:
+                //Any recoverable error. Display message to the user.
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+
+                break;
+        }
+    }
+
+}
